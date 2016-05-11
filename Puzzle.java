4e13321b1ad94febdee3310fe62b55c8482aa942
Miki K <mkinzy8@students.d125.org>
2016-05-11 10:51:36 -0500

    YOLOSWAG

--- Puzzle.java
+++ Puzzle.java
@@ -8,7 +8,9 @@
 public class Puzzle
 {
     // instance variables - replace the example below with your own
-    private int[][] puzzle;
+    int[][] base;
+    int[][] solution;
+    int[][] user;
     private int[][] puzzle1={{0,2,0,0,5,0,7,0,0},
             {9,0,0,7,0,1,0,6,0},
             {0,0,7,0,0,0,0,0,5},
@@ -18,11 +20,43 @@ public class Puzzle
             {8,0,0,0,0,0,4,0,0},
             {0,9,0,3,0,4,0,0,7},
             {0,0,3,0,2,0,0,1,0}};
-            
-    private int[][] puzzle2;
-    private int[][] puzzle3;
-    private int[][] puzzle4;
-    private int[][] puzzle5;
+
+    private int[][] puzzle2={{},
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { }};
+    private int[][] puzzle3={{},
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { }};
+    private int[][] puzzle4={{},
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { }};
+    private int[][] puzzle5={{},
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { }};
     private int[][] solution1={{1,2,8,6,5,3,7,9,4},
             {9,5,4,7,8,1,3,6,2},
             {6,3,7,9,4,2,1,8,5},
@@ -32,19 +66,53 @@ public class Puzzle
             {8,6,2,5,1,7,4,3,9},
             {5,9,1,3,6,4,8,2,7},
             {7,4,3,8,2,9,5,1,6}};
-    private int[][] solution2;
-    private int[][] solution3;
-    private int[][] solution4;
-    private int[][] solution5;
-    private int[][] userInput;
+    private int[][] solution2={{5,3,4,6,7,8,9,1,2},
+            {6,7,2,1,9,5,3,4,8},
+            {1,9,8,3,4,2,5,6,7},
+            { },
+            { },
+            { },
+            { },
+            { },
+            { }};
+    private int[][] solution3={{},
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { }};
+    private int[][] solution4={{},
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { }};
+    private int[][] solution5={{},
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { },
+            { }};
     /**
      * Constructor for objects of class Puzzle
      */
     public Puzzle()
     {
-        // initialise instance variables
-        int chosen=Math.random()*5+1;
+        //initialise instance variables
         
+        base=choosePuzzle();
+        solution=chooseSolution(base);
+        user=base;
+    
     }
 
     /**
@@ -56,14 +124,65 @@ public class Puzzle
     public int[][] getPuzzle()
     {
         // put your code here
-        
-        return puzzle;
+        return base;
     }
 
-    public int getSolutions(int x)
+    public int[][] getSolutions(int x)
     {
         // put your code here
-        return 0;
+        return solution;
+
+    }
+    public int[][] choosePuzzle()
+    {
+        // put your code here
+        int chosen=(int)Math.random()*5;
+        if (chosen==0){
+            return puzzle1;
+        }
+        else if (chosen==1){
+            return puzzle2;
+        }
+        else if (chosen==2){
+            return puzzle3;
+        }
+        else if (chosen==3){
+            return puzzle4;
+        }
+        else if (chosen==4){
+            return puzzle5;
+        }
+        else{
+            return null;
+        }
+    }
+     public int[][] chooseSolution(int[][] puzzle)
+    {
+        // put your code here
+        
+        if (puzzle==puzzle1){
+            return solution1;
+        }
+        else if (puzzle==puzzle2){
+            return solution2;
+        }
+        else if (puzzle==puzzle3){
+            return solution3;
+        }
+        else if (puzzle==puzzle4){
+            return solution4;
+        }
+        else if (puzzle==puzzle5){
+            return solution5;
+        }
+        else{
+            return null;
+        }
+    }
+    public void updateUser(int[][] p)
+    {
+        // put your code here
+        user=p;
 
     }
 }
