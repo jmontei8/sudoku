234bfc6303b2e78f40378c65b9f21735a50d5f7a
rloundy6 <rloundy6@students.d125.org>
2016-05-11 10:06:36 -0500

    checker test

---
+++ checkerRunner.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class checherrunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class checkerRunner
+{
+    public checkerRunner()
+    {
+
+    }
+}
